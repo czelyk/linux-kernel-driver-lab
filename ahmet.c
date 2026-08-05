84fa64546a9fb86b375b70b8b07a20b4152915fe
czelyk <ahmethassancelik@gmail.com>
2026-08-05 11:47:12 +0200

    Add device structure and initialization for multiple instances in ahmet driver

--- ahmet.c
+++ ahmet.c
@@ -14,11 +14,12 @@
         _IO(AHMET_IOCTL_MAGIC, 1)
 #define AHMET_GET_BUFFER_SIZE \
         _IOR(AHMET_IOCTL_MAGIC, 2, size_t)
+#define DEVICE_COUNT 4
+
 
 static dev_t dev_num;
-static struct cdev ahmet_cdev;
 static struct class *ahmet_class;
-static struct device *ahmet_device;
+static struct device *ahmet_device[DEVICE_COUNT];
 
 //static char buffer[BUFFER_SIZE];
 
@@ -31,6 +32,7 @@ static struct device *ahmet_device;
 
 struct ahmet_device
 {
+struct cdev cdev;
 char *buffer;
 size_t buffer_size;
 size_t buffer_capacity;
@@ -38,11 +40,20 @@ size_t buffer_capacity;
 struct mutex ahmet_mutex;
 };
 
-static struct ahmet_device *ahmet_dev;
+//static struct ahmet_device *ahmet_dev;
+
+static struct ahmet_device ahmet_devices[DEVICE_COUNT];
 
 static int ahmet_open(struct inode *inode, struct file *file)
 {
-    file->private_data = ahmet_dev;
+    struct ahmet_device *dev;
+
+    dev = container_of(inode->i_cdev,
+                        struct ahmet_device,
+                        cdev);
+
+    file->private_data = dev;
+
     pr_info("Device opened\n");
     return 0;
 }
@@ -182,19 +193,38 @@ static const struct file_operations fops = {
 static int __init ahmet_init(void)
 {
     int ret;
+    int i;
 
-    ahmet_dev = kzalloc(sizeof(*ahmet_dev), GFP_KERNEL);
-
-    if(!ahmet_dev)
+    for(i=0;i < DEVICE_COUNT;i++)
     {
-        pr_err("Failed to allocate device structure\n");
-        return -ENOMEM;
+        mutex_init(&ahmet_devices[i].ahmet_mutex);
+
+        ahmet_devices[i].buffer =
+            kzalloc(BUFFER_SIZE, GFP_KERNEL);
+    
+        if(!ahmet_devices[i].buffer)
+        {
+            pr_err("Failed to allocate buffer for device %d\n", i);
+            ret = -ENOMEM;
+            goto free_device_buffers;
+        }
+
+        ahmet_devices[i].buffer_capacity = BUFFER_SIZE;
+        ahmet_devices[i].buffer_size = 0;
     }
 
+    //ahmet_dev = kzalloc(sizeof(*ahmet_dev), GFP_KERNEL);
 
-    mutex_init(&ahmet_dev->ahmet_mutex);
+    //if(!ahmet_dev)
+    //{
+    //    pr_err("Failed to allocate device structure\n");
+    //    return -ENOMEM;
+    //}
 
-    ahmet_dev->buffer = kzalloc(BUFFER_SIZE, GFP_KERNEL);
+
+    //mutex_init(&ahmet_dev->ahmet_mutex);
+
+    //ahmet_dev->buffer = kzalloc(BUFFER_SIZE, GFP_KERNEL);
 
     //kzalloc alttaki kmalloc yapısının yerine geçiyor. 
     //normalde eski atıklarla dolu olan bir alan da tanımlanabilir.
@@ -203,113 +233,185 @@ static int __init ahmet_init(void)
     //ahmet_dev->buffer = kmalloc(BUFFER_SIZE, GFP_KERNEL);
     //memset(ahmet_dev->buffer, 0, BUFFER_SIZE);
 
-    if(!ahmet_dev->buffer)
-    {
-        pr_err("Failed to allocate buffer\n");
-        kfree(ahmet_dev);
-        ahmet_dev = NULL;        
-        return -ENOMEM;
-    }
+    //if(!ahmet_dev->buffer)
+    //{
+    //    pr_err("Failed to allocate buffer\n");
+    //    kfree(ahmet_dev);
+    //    ahmet_dev = NULL;        
+    //    return -ENOMEM;
+    //}
 
-    ahmet_dev->buffer_capacity = BUFFER_SIZE;
+    //ahmet_dev->buffer_capacity = BUFFER_SIZE;
     //ahmet_dev->buffer_size = 0;
     //memset(ahmet_dev->buffer, 0, ahmet_dev->buffer_capacity);
 
-    ret = alloc_chrdev_region(&dev_num, 0, 1, "ahmet");
+    ret = alloc_chrdev_region(&dev_num, 0, DEVICE_COUNT, "ahmet");
     if (ret < 0)
     {
         pr_err("Failed to allocate device number\n");
-        goto free_buffer;
+        goto free_device_buffers;
     }
 
     pr_info("Major: %d Minor: %d\n",
             MAJOR(dev_num),
             MINOR(dev_num));
 
-    cdev_init(&ahmet_cdev, &fops);
+    //cdev_init(&ahmet_dev->cdev, &fops);
 
-    ret = cdev_add(&ahmet_cdev, dev_num, 1);
-    if (ret < 0)
+    //ret = cdev_add(&ahmet_dev->cdev, dev_num, 1);
+    //if (ret < 0)
+    //{
+    //    pr_err("Failed to add cdev\n");
+    //    goto unregister_chrdev;
+    //}
+
+    for(i=0; i< DEVICE_COUNT; i++)
     {
-        pr_err("Failed to add cdev\n");
-        goto unregister_chrdev;
+        dev_t current_dev_num;
+
+        current_dev_num = MKDEV(MAJOR(dev_num),
+                                MINOR(dev_num)+ i);
+        
+        cdev_init(&ahmet_devices[i].cdev, &fops);
+
+        ret= cdev_add(&ahmet_devices[i].cdev,
+                                        current_dev_num,
+                                        1);
+
+        if (ret<0)
+        {
+            pr_err("Failed to add cdev for device %d\n", i);
+            goto unregister_cdevs;
+        }
     }
 
     ahmet_class = class_create("ahmet_class");
     if (IS_ERR(ahmet_class))
     {
         ret = PTR_ERR(ahmet_class);
-        goto unregister_cdev;
+        goto unregister_cdevs;
         //cdev_del(&ahmet_cdev);
         //unregister_chrdev_region(dev_num, 1);
         //return PTR_ERR(ahmet_class);
     }
 
-    ahmet_device = device_create(
+    for(i = 0;i < DEVICE_COUNT; i++)
+    {
+        dev_t current_dev_num;
+
+        current_dev_num = MKDEV(MAJOR(dev_num),
+                                MINOR(dev_num) + i);
+        
+        ahmet_device[i] = device_create(
         ahmet_class,
         NULL,
-        dev_num,
+        current_dev_num,
         NULL,
-        "ahmet"
+        "ahmet%d",
+        i
     );
 
-    if(IS_ERR(ahmet_device))
+    if(IS_ERR(ahmet_device[i]))
     {
-        ret = PTR_ERR(ahmet_device);
-        goto unregister_class;
+        ret = PTR_ERR(ahmet_device[i]);
+        pr_err("Failed to create device %d\n", i);
+        goto destroy_devices;
         //class_destroy(ahmet_class);
         //cdev_del(&ahmet_cdev);
         //unregister_chrdev_region(dev_num, 1);
         //return PTR_ERR(ahmet_device);
     }
+    }
+
 
     pr_info("Ahmet character device added\n");
 
     return 0;
 
-    unregister_class:
+    destroy_devices:
+        while (--i >=0)
+        {
+            dev_t current_dev_num;
+
+            current_dev_num = MKDEV(MAJOR(dev_num),
+                                    MINOR(dev_num) + i);
+
+            device_destroy(ahmet_class, current_dev_num);
+            ahmet_device[i] = NULL;
+        }
+
+        i = DEVICE_COUNT;
+
+
 
         class_destroy(ahmet_class);
 
-    unregister_cdev:
+    unregister_cdevs:
+        while(--i >=0)
+            cdev_del(&ahmet_devices[i].cdev);
 
-        cdev_del(&ahmet_cdev);
 
-    unregister_chrdev:
-        unregister_chrdev_region(dev_num, 1);
+        unregister_chrdev_region(dev_num, DEVICE_COUNT);
+        i= DEVICE_COUNT;
 
-    free_buffer:
-        kfree(ahmet_dev->buffer);
+    free_device_buffers:
+        while(--i >= 0)
+        {
+            mutex_destroy(&ahmet_devices[i].ahmet_mutex);
+            
+            kfree(ahmet_devices[i].buffer);
+            ahmet_devices[i].buffer = NULL;
 
-        kfree(ahmet_dev);
-        ahmet_dev = NULL;
-
+            ahmet_devices[i].buffer_capacity = 0 ;
+            ahmet_devices[i].buffer_size = 0;
+        }
+        
         return ret;
+        //kfree(ahmet_dev);
+        //ahmet_dev = NULL;
 }
 
 static void ahmet_cleanup(void)
 {
-    if(ahmet_device)
-        device_destroy(ahmet_class, dev_num);
+    int i;
+
+    //if(ahmet_device)
+    //    device_destroy(ahmet_class, dev_num);
+
+    for(i = 0; i < DEVICE_COUNT; i++)
+    {
+        dev_t current_dev_num;
+
+        current_dev_num = MKDEV(MAJOR(dev_num),
+                                MINOR(dev_num) + i);
+
+        device_destroy(ahmet_class, current_dev_num);
+        ahmet_device[i] = NULL;
+    }
 
 
     if(ahmet_class)
         class_destroy(ahmet_class);
 
-    cdev_del(&ahmet_cdev);
+    //cdev_del(&ahmet_dev->cdev);
 
-    unregister_chrdev_region(dev_num, 1);
-
-
-    if(ahmet_dev)
+    for(i =0;i < DEVICE_COUNT;i++)
     {
-        mutex_destroy(&ahmet_dev->ahmet_mutex);
+        cdev_del(&ahmet_devices[i].cdev);
+    }
 
-        kfree(ahmet_dev->buffer);
+    unregister_chrdev_region(dev_num, DEVICE_COUNT);
 
-        kfree(ahmet_dev);
 
-        ahmet_dev = NULL;
+    for(i=0; i< DEVICE_COUNT;i++)
+    {
+        mutex_destroy(&ahmet_devices[i].ahmet_mutex);
+
+        kfree(ahmet_devices[i].buffer);
+        ahmet_devices[i].buffer = NULL;
+
+        ahmet_devices[i].buffer_capacity = 0;
+        ahmet_devices[i].buffer_size = 0;
     }
 }
 
---
+++ ekler/lehrergesagt.md
@@ -0,0 +1,1 @@
+sistem çağrıları da yazılacak. 
